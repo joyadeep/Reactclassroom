aff1440602cbcba3424e13ff6a0f49661a9489d6
joyadeep <joyadeeplimbu@gmail.com>
2023-05-03 20:42:30 +0545

    second commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import {useState} from 'react'
 import logo from './logo.svg';
 
 
@@ -5,9 +6,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [number,setNumber]=useState(0);
+
+  // const handleClick=()=>{
+  //   setNumber(number+1);
+  // }
   return (
     <div className="App" >
-      <h1>hello world</h1>
+      {
+        console.log("number ==",number)
+      }
+      <h1 style={{fontSize:"52px"}}> {number} </h1>
+      <button
+       style={{fontSize:"20px",backgroundColor:"green",color:"white",padding:"5px",border:"0",borderRadius:"9px",cursor:"pointer"}}
+       
+       onClick={()=>{setNumber(number+1)}}
+       >
+        Increment</button>
     </div>
   );
 }
