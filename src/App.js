318e9e3a5b00cb27254b81261d0270a0474716a3
joyadeep <joyadeeplimbu@gmail.com>
2023-05-04 20:49:43 +0545

    updated something

--- src/App.js
+++ src/App.js
@@ -6,10 +6,26 @@ import Counter from './pages/Counter';
 import './App.css';
 
 function App() {
+const [data,setData]=useState({name:"",email:""})
 
+const handleChange=(e)=>{
+  console.log("event name=",e.target.name)
+  console.log("event value = ",e.target.value)
+  console.log("---data ==",...data)
+  setData({...data,[e.target.name]:e.target.value})
+}
+const handleSubmit=(e)=>{
+  e.preventDefault();
+  console.log("data ==",data)
+}
   return (
     <div className="App" >
-      <Counter/>
+      {/* <Counter/> */}
+      <form onSubmit={handleSubmit}>
+          <input type="text" name="name" value={data.name} onChange={handleChange} />
+          <input type="email" name="email" value={data.email} onChange={handleChange} />
+          <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
