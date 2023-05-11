77bb9897e9303fd8a0360341075721b369e6dbb8
joyadeep <joyadeeplimbu@gmail.com>
2023-05-11 20:43:08 +0545

    responsive react

--- src/App.js
+++ src/App.js
@@ -1,31 +1,20 @@
 import {useState} from 'react'
 import logo from './logo.svg';
+import User from "./components/User"
 
 
 
 import './App.css';
+import Customer from './pages/Customer';
+import Admin from './pages/Admin';
 
 function App() {
 
-  const [number,setNumber]=useState(0);
-
-  // const handleClick=()=>{
-  //   setNumber(number+1);
-  // }
+  
   return (
     <div className="App" >
-      {
-        console.log("number ==",number)
-      }
-      <h1 style={{fontSize:"52px"}}> {number} </h1>
-      <button
-       style={{fontSize:"20px",backgroundColor:"green",color:"white",padding:"5px",border:"0",borderRadius:"9px",cursor:"pointer"}}
-       
-       onClick={()=>{setNumber(number+1)}}
-       >
-        Increment</button>
-
-
+       <Customer />
+       <Admin/>
     </div>
   );
 }
---
+++ src/pages/Customer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import User from '../components/User'
+
+const Customer = () => {
+  return (
+    <div>
+        <User name="user" email="user@gmail.com" >
+            <h1>This is customer page</h1>
+        </User>
+    </div>
+  )
+}
+
+export default Customer
