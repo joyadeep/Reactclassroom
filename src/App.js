5438fe15d36564546e8722173b73e04f6baf59f2
joyadeep <joyadeeplimbu@gmail.com>
2023-07-10 10:49:05 +0545

    configured tailwind css

--- src/App.js
+++ src/App.js
@@ -1,31 +1,9 @@
-import {useState} from 'react'
-import logo from './logo.svg';
-
-
-
-import './App.css';
-
 function App() {
 
-  const [number,setNumber]=useState(0);
-
-  // const handleClick=()=>{
-  //   setNumber(number+1);
-  // }
   return (
-    <div className="App" >
-      {
-        console.log("number ==",number)
-      }
-      <h1 style={{fontSize:"52px"}}> {number} </h1>
-      <button
-       style={{fontSize:"20px",backgroundColor:"green",color:"white",padding:"5px",border:"0",borderRadius:"9px",cursor:"pointer"}}
-       
-       onClick={()=>{setNumber(number+1)}}
-       >
-        Increment</button>
-
-
+    <div >
+      <h1 className='text-center text-6xl font-semibold'>Start your project here</h1>
+      <h4 className="text-center text-xl mt-3">fonts not included !</h4>
     </div>
   );
 }
